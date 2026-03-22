0bbd5f918ffdb989b7e632e3b43b45716d7fcc6d
DIRCEULH <dirceulh@hotmail.com>
2026-03-22 17:30:43 -0300

    Implementado e a ajustado datas

--- server.js
+++ server.js
@@ -22,25 +22,109 @@ db.connect((err) => {
   }
 });
 
+function formatToMySQLDateTime(dataBR) {
+  if (!dataBR) return null;
+
+  try {
+    const [datePart, timePart] = dataBR.split(' ');
+
+    if (!datePart || !timePart) return null;
+
+    const [day, month, year] = datePart.split('/');
+    const [hour, minute] = timePart.split(':');
+
+    if (!day || !month || !year || !hour || !minute) return null;
+
+    return `${year}-${month}-${day} ${hour}:${minute}:00`;
+  } catch (e) {
+    console.error('Erro ao converter data:', dataBR);
+    return null;
+  }
+}
+
 
 // cadastro
 app.post("/register", (req, res) => {
 
   const { email, password } = req.body;
 
-  const sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+  const query = "SELECT * FROM users WHERE email = ? ";
 
-  db.query(sql, [email, password], (err, result) => {
+  db.query(query, [email, password], (err, result) => {
 
     if (err) {
-      return res.status(500).json({ message: "Erro ao cadastrar" });
+      return res.status(500).json({ message: "Erro no servidor" });
     }
 
-    res.json({ message: "Usuário cadastrado com sucesso" });
+    if (result.length > 0) {
+
+      res.json({ message: "Já existe esse email cadastrado!" });
+
+    } else {
+
+      const sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+
+      db.query(sql, [email, password], (err, result) => {
+
+        if (err) {
+          return res.status(500).json({ message: "Erro ao cadastrar" });
+        }
+
+
+        res.json({ message: "Usuário cadastrado com sucesso!" });
+
+      });
+
+    }
 
   });
 
+
 });
+
+// cadastro Visitantes
+app.post("/visitors", (req, res) => {
+
+  const {
+    cpf_cnpj,
+    nome,
+    empresa,
+    data_entrada,
+    data_saida,
+    placa,
+    destino,
+    atendente,
+    obs
+  } = req.body;
+
+    const dataEntradaMySQL = formatToMySQLDateTime(data_entrada);
+    const dataSaidaMySQL = formatToMySQLDateTime(data_saida);
+
+   // res.json({ message: dataEntradaMySQL + dataEntradaMySQL});
+
+  const sql = `
+    INSERT INTO visitors 
+    (cpf_cnpj, nome, empresa, data_entrada, data_saida, placa, destino, atendente, obs)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(
+    sql,
+    [cpf_cnpj, nome, empresa, dataEntradaMySQL, dataSaidaMySQL, placa, destino, atendente, obs],
+    (err, result) => {
+
+      if (err) {
+        console.error(err);
+        console.log('BODY COMPLETO:', req.body);
+        return res.status(500).json({ message: "Erro ao cadastrar visitante" });
+      }
+   
+     res.json({ message: "Visitante cadastrado com sucesso!"});
+    }
+  );
+});
+
+
 
 
 // login
