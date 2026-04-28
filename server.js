7f31869faebd471554ed5bf4f428dd1b6ecd1195
DIRCEULH <dirceulh@hotmail.com>
2026-04-28 20:00:27 -0300

    Ajuste cadastro ativo Inativo

--- server.js
+++ server.js
@@ -165,9 +165,9 @@ app.post("/login", (req, res) => {
 
   const {  email, password } = req.body;
 
-  const sql = "SELECT * FROM users WHERE email = ? AND password = ? AND STATUS = 'A' ";
+  const sql = "SELECT * FROM users WHERE email = ? AND password = ? AND STATUS = ? ";
 
-  db.query(sql, [email, password], (err, result) => {
+  db.query(sql, [email, password,'A'], (err, result) => {
 
     if (err) {
       return res.status(500).json({ message: "Erro no servidor" });
