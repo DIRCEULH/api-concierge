1cdcaeb596c7509aad387a5148684c2d4a2a7e16
DIRCEULH <dirceulh@hotmail.com>
2026-05-12 12:07:26 -0300

    teste Conexao porta

--- server.js
+++ server.js
@@ -683,6 +683,7 @@ app.get("/debug", (req, res) => {
   res.json({
     DB_HOST: process.env.DB_HOST,
     DB_USER: process.env.DB_USER,
+    DB_PORT: process.env.DB_PORT,
     DB_NAME: process.env.DB_NAME,
     DB_PASSWORD: process.env.DB_PASSWORD ? "OK (oculto)" : null
   });
