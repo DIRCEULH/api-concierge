859a3eb6b9f333be20df9fab57396003a96d4ffb
DIRCEULH <dirceulh@hotmail.com>
2026-05-12 10:49:58 -0300

    Ajuste arquivo env

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-import dotenv from "dotenv";
-dotenv.config();
-
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
